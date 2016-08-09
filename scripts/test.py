0679f4b2b5425fa1e1c09e24468f3d57f70faa6b
Christopher Coffey <nomad@cybermerc.org>
2016-08-09 16:14:21 -0500

    more docs

--- scripts/test.py
+++ scripts/test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 Parse users from IAM and verify that password and
-access key are less than 90 days old
+access key are less than X days old
 """
 
 import boto3
@@ -10,17 +10,17 @@ import botocore
 client = boto3.client('iam')
 
 x = client.list_users()
-a = []
 
 
 def user_list():
     ''' Create list of IAM users by username '''
+    a = []
     for i in x['Users']:
         a.append(i['UserName'])
     return a
 
 
-def last_passwd_use():
+def passwd_last_used():
     '''
     create dict of IAM accounts that have last used time stamp on password
     '''
@@ -39,11 +39,8 @@ def passwd_creation_date():
     for i in x['Users']:
         try:
             r = client.get_login_profile(UserName=i['UserName'])
-            print(r)
             if 'CreateDate' in r['LoginProfile']:
-                print(r['LoginProfile']['CreateDate'])
+                d[i['UserName']] = r['LoginProfile']['CreateDate']
         except botocore.exceptions.ClientError:
             pass
     return d
-
-print(passwd_creation_date())
